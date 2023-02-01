631a69f81e56bece8eddde8acf5eed731aad4523
gkintana <81775867+gkintana@users.noreply.github.com>
2023-02-01 12:58:01 +0400

    update mariadb sql file to add user in database

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 service mysql start
-exec "$@"
+mysql -u root
+# exec "$@"
--- srcs/requirements/mariadb/initialize_database.sql
+++ srcs/requirements/mariadb/initialize_database.sql
@@ -1,3 +1,7 @@
-CREATE DATABASE test;
+CREATE DATABASE 42_wordpress_db;
 
-CREATE DATABASE test2;
+CREATE USER 'test_user'@'localhost' IDENTIFIED BY 'password';
+
+GRANT ALL PRIVILEGES ON 42_wordpress_db.* to 'test_user'@'localhost';
+
+FLUSH PRIVILEGES;
