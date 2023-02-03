cbd95a5d330d22201f0495be7c22e9244da75aa4
gkintana <81775867+gkintana@users.noreply.github.com>
2023-02-04 00:23:50 +0400

    modify wordpress & mariadb in an attempt to fix database error

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+
+# Start the MariaDB service
 service mysql start
-# mysql -u root
-# exec "$@"
+
+# Wait until the database is up and running
+while ! mysqladmin ping -h localhost --silent; do
+    sleep 1
+done
+
+# Create the database and user if they do not exist
+mysql -e "CREATE DATABASE IF NOT EXISTS test_db;"
+mysql -e "CREATE USER IF NOT EXISTS 'test_user'@'localhost' IDENTIFIED BY 'password';"
+mysql -e "GRANT ALL PRIVILEGES ON test_db.* TO 'test_user'@'localhost';"
+mysql -e "FLUSH PRIVILEGES;"
+
+# Keep the MariaDB service running
+# tail -f /dev/null
 mysqld
--- srcs/requirements/mariadb/initialize_database.sql
+++ srcs/requirements/mariadb/initialize_database.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE 42_wordpress_db;
+CREATE DATABASE test_db;
 
 CREATE USER 'test_user'@'localhost' IDENTIFIED BY 'password';
 
-GRANT ALL PRIVILEGES ON 42_wordpress_db.* to 'test_user'@'localhost';
+GRANT ALL PRIVILEGES ON test_db.* to 'test_user'@'localhost';
 
 FLUSH PRIVILEGES;
