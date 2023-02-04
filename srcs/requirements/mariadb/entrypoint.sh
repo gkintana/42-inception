09af690785d7c2870a83a5bfad4bc1daed7c93be
gkintana <81775867+gkintana@users.noreply.github.com>
2023-02-05 01:32:12 +0400

    update mariadb container

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -9,11 +9,11 @@ while ! mysqladmin ping -h localhost --silent; do
 done
 
 # Create the database and user if they do not exist
-mysql -e "CREATE DATABASE IF NOT EXISTS test_db;"
-mysql -e "CREATE USER IF NOT EXISTS 'test_user'@'localhost' IDENTIFIED BY 'password';"
-mysql -e "GRANT ALL PRIVILEGES ON test_db.* TO 'test_user'@'localhost';"
-mysql -e "FLUSH PRIVILEGES;"
+mysql -e "CREATE DATABASE IF NOT EXISTS test_db;" || { echo "Error creating database test_db" ; exit 1; }
+mysql -e "CREATE USER IF NOT EXISTS 'test_user'@'localhost' IDENTIFIED BY 'password';" || { echo "Error creating user test_user" ; exit 1; }
+mysql -e "GRANT ALL PRIVILEGES ON test_db.* TO 'test_user'@'localhost';" || { echo "Error granting privileges to test_user" ; exit 1; }
+mysql -e "FLUSH PRIVILEGES;" || { echo "Error flushing privileges" ; exit 1; }
 
 # Keep the MariaDB service running
-# tail -f /dev/null
-mysqld
+tail -f /dev/null
+# mysqld
--- srcs/requirements/mariadb/initialize_database.sql
+++ srcs/requirements/mariadb/initialize_database.sql
@@ -1,7 +1,4 @@
 CREATE DATABASE test_db;
-
 CREATE USER 'test_user'@'localhost' IDENTIFIED BY 'password';
-
 GRANT ALL PRIVILEGES ON test_db.* to 'test_user'@'localhost';
-
 FLUSH PRIVILEGES;
