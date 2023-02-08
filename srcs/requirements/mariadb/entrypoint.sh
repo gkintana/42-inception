53d4332f3d1e0ed5886b1adb8ce3683ed78ad79a
gkintana <81775867+gkintana@users.noreply.github.com>
2023-02-09 01:07:03 +0400

    fixed database error

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-service mysql start
+# service mysql start
 
+if [ -f /tmp/initialize_database.sql ]; then
 mysql -e "$(eval "echo \"$(cat /tmp/initialize_database.sql)\"")"
-rm /tmp/initialize_database.sql
+# rm /tmp/initialize_database.sql
+fi
 
-tail -f /dev/null
+
+
+# tail -f /dev/null
 # mysqld
+exec $@
---
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -f /var/www/html/wp-config-sample.php ]; then
+
+mv wp-config-sample.php wp-config.php
+
+sed -i 's/database_name_here/wordpress_service/' /var/www/html/wp-config.php && \
+sed -i 's/username_here/sql_user/' /var/www/html/wp-config.php && \
+sed -i 's/password_here/123password123/' /var/www/html/wp-config.php && \
+sed -i 's/localhost/mariadb/' /var/www/html/wp-config.php && \
+sed -i 's|listen = /run/php/php7.3-fpm.sock|listen = 0.0.0.0:9000|' /etc/php/7.3/fpm/pool.d/www.conf
+
+wp core install --allow-root --url=test.com --title="TEST" --admin_user=wpcli --admin_password=wpcli --admin_email=info@wp-cli.org
+fi
+
+exec $@
